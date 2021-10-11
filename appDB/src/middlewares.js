ec11535372888ebebc2588494497e21c902bd0bd
Umar Nasir <umarnasir335@gmail.com>
2021-10-10 21:35:19 -0700

    updated middlewares

--- appDB/src/middlewares.js
+++ appDB/src/middlewares.js
@@ -1,21 +1,32 @@
+const errorTypes = {
+  ValidationError: 422,
+  UniqueViolationError: 409,
+};
+
+const errorMessages = {
+  UniqueViolationError: 'Already exists.',
+};
+
 function notFound(req, res, next) {
+  const error = new Error(`Not found - ${req.originalUrl}`);
   res.status(404);
-  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
   next(error);
 }
 
-/* eslint-disable no-unused-vars */
-function errorHandler(err, req, res, next) {
-  /* eslint-enable no-unused-vars */
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+// eslint-disable-next-line no-unused-vars
+function errorHandler(error, req, res, next) {
+  const statusCode =
+    res.statusCode === 200 ? errorTypes[error.name] || 500 : res.statusCode;
   res.status(statusCode);
   res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+    status: statusCode,
+    message: errorMessages[error.name] || error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+    errors: error.errors || undefined,
   });
 }
 
 module.exports = {
   notFound,
-  errorHandler
+  errorHandler,
 };
